7440125fe19a9b314a6a8a6ce265f57d16958b71
Lisa Herzberg <lisaherzberg@Lisas-MacBook-Pro.local>
2024-07-24 13:59:05 -0600

    fixed margins

--- src/components/Description.tsx
+++ src/components/Description.tsx
@@ -15,14 +15,6 @@ export const Decscription: React.FC = () =>  {
         I believe in the power of capturing joyful moments and heartfelt laughter that make life special, while highlighting the unique personality and spirit of each subject. 
         
  Let me help you preserve your most cherished moments with photography that brings meaning and happiness to your memories for a lifetime.</p>
-
-            {/* <p>
-                My passion lies in capturing the smiles and laughter that make life special while
-                 bringing out the unique personality and spirit of each subject.
-                delivering photographs that reflect your story with vibrancy and sincerity. 
-                Let me help you preserve your most cherished moments with photography that
-                 brings joy and warmth to your memories for a lifetime.
-            </p> */}
         </div>
     );
 };
